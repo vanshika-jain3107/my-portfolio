08809013557495f9f720aebfe56650d374b3c154
vanshika-jain3107 <148933789+vanshika-jain3107@users.noreply.github.com>
2025-06-11 17:42:11 +0530

    Change text of HeroSection

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <div className='container max-w-4xl mx-auto text-center z-10 '>
             <div className='space-y-6'>
                 <h1 className='text-4xl md:text-6xl font-bold tracking-tight'>
-                    <span className='opacity-0 animate-fade-in-delay-1'>Hi I'm </span>
+                    <span className='opacity-0 animate-fade-in-delay-1'>Hello I'm </span>
                     <span className='text-primary opacity-0 animate-fade-in-delay-2'>Vanshika </span>
                     <span className='text-gradient ml-2 opacity-0 animate-fade-in-delay-3'>{" "}Jain </span>
                 </h1>
